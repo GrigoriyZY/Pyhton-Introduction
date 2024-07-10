90dc7281cfd4d6fccb0408c67bf86aa66d5fb269
GrigoriyZY <mymail@gzyryanov.ru>
2024-07-10 14:02:49 +0500

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,10 @@
+# Работа с переменными
+
+# Задаем переменные, задания 1-4
+number_of_homeworks = 12
+hours_spent_total = 1.5
+course_name = "Python"
+time_per_homework = hours_spent_total / number_of_homeworks
+
+# Выводим результат
+print ("Курс:",course_name,", всего задач:",number_of_homeworks,', затрачено часов:',hours_spent_total,", среднее время выполнения:",time_per_homework,".")
