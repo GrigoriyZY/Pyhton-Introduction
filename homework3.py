7006f70b74e5beaff90615bc28474b86925ea985
GrigoriyZY <mymail@gzyryanov.ru>
2024-07-11 14:38:19 +0500

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,9 @@
+# Динамическая типизация
+name = "Grigoriy"
+print ('Name:',name)
+age = 45
+print ('Age:',age)
+age = age + 3
+print ('New age:',age)
+is_student = True
+print ('Is Student:',is_student)
