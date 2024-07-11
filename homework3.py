1d94f640e805088d4106b7b5f84311971b689afe
GrigoriyZY <mymail@gzyryanov.ru>
2024-07-11 14:43:20 +0500

    homework3.py

--- homework3.py
+++ homework3.py
@@ -1,9 +1,9 @@
 # Динамическая типизация
 name = "Grigoriy"
-print ('Name:',name)
+print('Name:', name)
 age = 45
-print ('Age:',age)
+print('Age:', age)
 age = age + 3
-print ('New age:',age)
+print('New age:', age)
 is_student = True
-print ('Is Student:',is_student)
+print('Is Student:', is_student)
