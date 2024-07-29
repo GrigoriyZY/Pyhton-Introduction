f25f933c047760555c7311cfb65ffbeeeddd5da8
GrigoriyZY <mymail@gzyryanov.ru>
2024-07-29 07:41:10 +0500

    Add files via upload

---
+++ module_3_5.py
@@ -0,0 +1,17 @@
+# Реверсивное умножение цифр
+
+
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0:1])
+    print(str_number)
+    print(first)
+    if len(str_number[1:]) >= 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    elif len(str_number[1:]) < 1:
+        return first
+
+
+n = int(input('Введите произвольное число:'))
+result = get_multiplied_digits(n)
+print(result)
